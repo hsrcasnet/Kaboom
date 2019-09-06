9e015273675c2b8fd58dff9a5fb25fc7122e4925
Thomas Galliker <thomas@bluewin.ch>
2019-09-06 15:49:19 +0200

    Optimize console output, add more comments for DEMO

--- Kaboom/Program.cs
+++ Kaboom/Program.cs
@@ -4,13 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Unhandled Exception
-        throw new Exception("Kaboom! This exception is not handled by our application!");
+        // DEMO: Attach to UnhandledException event and see what happens
+        // in case an exception is thrown and noone cares about it...
+        //AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+
+        // DEMO: Throw an unhandled exception
+        //throw new Exception("Kaboom! This exception is not handled by our application!");
 
         try
         {
             // Handled Exception
-            throw new Exception("Kaboom! This exception is handled by our application!");
+            //throw new Exception("Kaboom! This exception is handled by our application!");
 
             // Demonstrate first chance exception with inner exceptions
             KaboomSettings.SetValue(null, null);
@@ -22,51 +26,25 @@ class Program
         catch (Exception e)
         {
             Console.WriteLine(e);
+
+            // DEMO: Rethrow a handled exception to become unhandled
+            // Rethrow the catched exception makes it become 'unhandled'
+            // because noone else outside this try-catch is caring about it
+            //throw;
         }
 
+        Console.WriteLine();
+        Console.WriteLine($"The program finished successfully");
+        Console.WriteLine("Press Enter to continue");
         Console.ReadKey();
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //static void Main(string[] args)
-    //{
-    //    AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
-    //    throw new Exception("Kaboom");
-    //}
-
-
-    //private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
-    //{
-    //    Console.WriteLine(e.ExceptionObject.ToString());
-    //    Console.WriteLine("Press Enter to continue");
-    //    Console.ReadLine();
-    //    Environment.Exit(1);
-    //}
+    private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"The program crashed with following exception: {e.ExceptionObject.ToString()}");
+        Console.WriteLine("Press Enter to continue");
+        Console.ReadLine();
+        Environment.Exit(1);
+    }
 }
